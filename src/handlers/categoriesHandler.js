4eea8b4c357b04ab47569fafb0e83c9c4ae407e7
Bernardo Heduvan <berni.heduvan1@hotmail.com>
2023-10-25 19:36:14 -0300

    cree la ruta para una nueva categoria

--- src/handlers/categoriesHandler.js
+++ src/handlers/categoriesHandler.js
@@ -1,5 +1,6 @@
 const {
   getCategories_Controller,
+  createCategory_Controller
 } = require("../controllers/categories/categoriesController");
 
 const getCategories_Handler = async (req, res) => {
@@ -11,6 +12,20 @@ const getCategories_Handler = async (req, res) => {
   }
 };
 
+const createCategory_Handler = async (req, res) => {
+  try {
+    const data = req.body;
+    if (!data.name) return res.status(400).json("Missing data");
+    console.log(data)
+    
+    const newCategory = await createCategory_Controller(data);
+    return res.status(201).json(newCategory); // Devuelve la categoría creada en el handler
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getCategories_Handler,
+  createCategory_Handler,
 };
