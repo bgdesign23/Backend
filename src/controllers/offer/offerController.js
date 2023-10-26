fbd055e6bf4d481c0c90331bd97cd531870c2106
Bernardo Heduvan <berni.heduvan1@hotmail.com>
2023-10-26 12:20:13 -0300

    agregue un post para crear una oferta

--- src/controllers/offer/offerController.js
+++ src/controllers/offer/offerController.js
@@ -1,4 +1,4 @@
-const { Offer, Product } = require("../../db");
+const { Offer, Product, User } = require("../../db");
 
 const getOfferController = async () => {
   const data = await Offer.findAll();
@@ -9,6 +9,39 @@ const getOfferController = async () => {
   return data;
 }
 
+const createNewOffer_controller = async (data, id) => {
+  
+  try { 
+    const findOffer = await Offer.findOne({
+      where : { 
+          title: data.title,
+          description: data.description,
+      },    
+  })
+  if(findOffer)
+  return res.status(302).json( { message: "this offer already exists" });
+    
+  const findUser = await User.findOne({
+      where : { 
+          id: id,
+      },    
+  });
+  if(!findUser) 
+  throw new Error ("no existe un usario");
+     
+  const offerObj = {
+    title: data.title,
+    description: data.description,
+  }
+  const newOffer = await Offer.create(offerObj);
+  await newOffer.setUser(findUser);
+  return newOffer
+} catch (error) {
+  throw new Error(error.message);
+}
+}
+
 module.exports = {
     getOfferController,
+    createNewOffer_controller
 }
--- src/routes/offerRoutes.js
+++ src/routes/offerRoutes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const {
     getOfferHandler,
+    postOffer_handler,
 } = require("../handlers/offerHandler");
 const offerRouter = Router();
 
 // RUTAS GET;
 offerRouter.get("/", getOfferHandler);
+// RUTA POST;
+offerRouter.post("/create/:id", postOffer_handler)
 
 module.exports = offerRouter;
