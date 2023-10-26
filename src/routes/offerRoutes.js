3a671f9c8e3e23c69faf222b27cf174fb5b77645
Bernardo Heduvan <berni.heduvan1@hotmail.com>
2023-10-26 18:24:13 -0300

    CRUD de ofertas Completo

--- src/routes/offerRoutes.js
+++ src/routes/offerRoutes.js
@@ -2,12 +2,21 @@ const { Router } = require("express");
 const {
     getOfferHandler,
     postOffer_handler,
+    getOfferByIdHandler,
+    deleteOfferHandler, 
+    // getOfferByNameHandler,
 } = require("../handlers/offerHandler");
 const offerRouter = Router();
 
 // RUTAS GET;
 offerRouter.get("/", getOfferHandler);
+offerRouter.get("/:id", getOfferByIdHandler);
+// offerRouter.get("/search/:name", getOfferHandler);
+
 // RUTA POST;
-offerRouter.post("/create/:id", postOffer_handler)
+offerRouter.post("/create/:id", postOffer_handler);
+
+// RUTA DELETE;
+offerRouter.delete("/delete/:id", deleteOfferHandler);
 
 module.exports = offerRouter;
