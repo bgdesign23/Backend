dc61e3f84f5cefdbf6475b87ad7e186a5a0e4bfa
Daniel Tello <daniel.qui.tello@gmail.com>
2023-11-13 16:50:25 -0500

    products_controller

--- src/utils/toggleUrl.js
+++ src/utils/toggleUrl.js
@@ -1,5 +1,5 @@
-// const BACK_URL = "http://localhost:3001";
-// const FRONT_URL = "http://localhost:5173";
-const BACK_URL = "https://backend-muebles.vercel.app";
-const FRONT_URL = "https://blackgroupdesing.vercel.app";
+const BACK_URL = "http://localhost:3001";
+const FRONT_URL = "http://localhost:5173";
+// const BACK_URL = "https://backend-muebles.vercel.app";
+// const FRONT_URL = "https://blackgroupdesing.vercel.app";
 module.exports = { BACK_URL, FRONT_URL };
